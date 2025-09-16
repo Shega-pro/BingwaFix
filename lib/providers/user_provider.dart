caea2f41ae53980d1d5a1248bdd48bf6e3198806
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-09-16 20:20:26 +0300

    trying to setup user login rendering of data from database

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 import '../model/user.dart';
 
 class UserProvider extends ChangeNotifier {
-  User? _user;
+  User? _user; //Holds the logged -in user
 
-  User? get user => _user;
+  User? get user => _user; // expose user
+  bool get isAuthenticated => _user != null; //quick check
 
-  void setUser(User user) {
-    _user = user;
-    notifyListeners(); //Tells the app to refresh when the user changes
+//   Simulate log in
+  void login(String Id, String fname, String lname) {
+    _user = User(Id: Id, fname: fname, lname: lname);
+    notifyListeners(); //Notify UI that the user has changed
+
   }
 
+  // logout
+  void logout() {
+    _user = null;
+    notifyListeners();
+  }
 }
