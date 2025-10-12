75cf444d01cfbcf19221a5a1a938597fc514adfe
JOHNELLY-001 <johnkiranga9@gmail.com>
2025-10-12 22:46:48 +0300

    Changes on the text styling and buttons

--- lib/Registration/FundiRegister.dart
+++ lib/Registration/FundiRegister.dart
@@ -8,37 +8,40 @@ class FundiAuthPortal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Contact BingwaFix Offices for Registration', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600,),),
-            SizedBox(height: 08,),
-            Text('+255 629 763 121', style: TextStyle(fontSize: 15, color: Colors.blueGrey),),
-            SizedBox(height: 08,),
-            Text('+255 628 729 335', style: TextStyle(fontSize: 15, color: Colors.blueGrey),),
-            SizedBox(height: 08,),
-            Text('fixflowinnovators@gmail.com', style: TextStyle(fontSize: 15, color: Colors.blue),),
-            SizedBox(height: 100,),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => FundiLoginPage()));
-                },
-              child: const Text('Sign In',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600,color: Colors.white),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(color: Colors.white60, width: 1),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Contact BingwaFix Offices for Registration', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600,),),
+              SizedBox(height: 04,),
+              Text('+255 629 763 121', style: TextStyle(fontSize: 14, color: Colors.black),),
+              SizedBox(height: 04,),
+              Text('+255 628 729 335', style: TextStyle(fontSize: 14, color: Colors.black),),
+              SizedBox(height: 04,),
+              Text('fixflowinnovators@gmail.com', style: TextStyle(fontSize: 14, color: Colors.blue),),
+              SizedBox(height: 40,),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => FundiLoginPage()));
+                  },
+                child: const Text('Sign In',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600,color: Colors.white),
                 ),
-                backgroundColor: Colors.blueGrey,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    side: BorderSide(color: Colors.white60, width: 1),
+                  ),
+                  backgroundColor: Colors.green,
+                  padding: EdgeInsets.symmetric(horizontal: 28, vertical: 10),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
